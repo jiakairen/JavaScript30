06d83f5075bb18b4201591ebb136903ab5dadc10
Jiakai Ren <jiakai.ren@gmail.com>
2022-12-30 22:07:51 +1100

    unlimited unicorns

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -9,8 +9,6 @@ const ranges = player.querySelectorAll(".player__slider");
 
 // Build out functions
 function togglePlay() {
-  // const method = video.paused ? 'play' : 'pause';
-  // video[method]();
   video.paused ? video.play() : video.pause();
 }
 
